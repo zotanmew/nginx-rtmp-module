753442dad1efbcf88ef5dbc089eb95c4f3992710
Sergey Dryabzhinsky <sergey@rusoft.ru>
2016-02-28 18:40:37 +0300

    Fix fucntion definition

--- ngx_rtmp.h
+++ ngx_rtmp.h
@@ -589,7 +589,7 @@ ngx_int_t ngx_rtmp_send_close_method(ngx_rtmp_session_t *s, char *methodName);
 ngx_int_t ngx_rtmp_send_fcpublish(ngx_rtmp_session_t *s, u_char *desc);
 ngx_int_t ngx_rtmp_send_fcunpublish(ngx_rtmp_session_t *s, u_char *desc);
 ngx_int_t ngx_rtmp_send_fi(ngx_rtmp_session_t *s);
-ngx_int_t ngx_rtmp_send_bwcheck(ngx_rtmp_session_t *s, u_char *payload);
+ngx_int_t ngx_rtmp_send_bwcheck(ngx_rtmp_session_t *s, u_char *payload, size_t plength);
 ngx_int_t ngx_rtmp_send_bwdone(ngx_rtmp_session_t *s,
                                double kbitDown, ngx_uint_t deltaDown, double deltaTime, ngx_msec_t latency);
 ngx_int_t ngx_rtmp_send_onclientbwcheck(ngx_rtmp_session_t *s, double inTrans,
